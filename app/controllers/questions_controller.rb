4445309dc3bf5b480684d366e2495f96d9e85696
kat3185 <Ken.Thomas.3185@gmail.com>
2015-06-22 17:54:06 -0400

    added new and create functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,24 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.where(id: params[:id]).first
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(drink_params)
+    if @question.save
+      flash[:notice] = 'Your question was submitted.'
+      redirect_to questions_path
+    else
+      flash[:notice] = 'Title must be 40 characters, description must be 150.'
+      render :new
+    end
+  end
+
+  protected
+  def drink_params
+    params.require(:question).permit(:title, :description)
+  end
 end
---
+++ spec/features/create_new_question_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+# - [x] I must provide a title that is at least 40 characters long
+# - [x] I must provide a description that is at least 150 characters long
+# - [x] I must be presented with errors if I fill out the form incorrectly
+
+feature 'post a new question', %Q{
+  As a user
+  I want to post a question
+  So that I can receive help from others
+} do
+
+  scenario 'post a new question' do
+    visit new_question_path
+    fill_in 'Title', with: "This is a test question so I really hope this works"
+    fill_in 'Description', with: "this is just unreasonably repetative this is just unreasonably repetative this is just unreasonably repetative this is just unreasonably repetative this is just unreasonably repetative this is just unreasonably repetative this is just unreasonably repetative this is just unreasonably repetative "
+    click_button 'Add Question'
+    expect(page).to have_content("Your question was submitted.")
+    expect(page).to have_content(Question.first.title)
+  end
+
+  scenario 'try to post an invalid question' do
+    visit new_question_path
+    fill_in 'Title', with: "Failure"
+    fill_in 'Description', with: "Sheesh"
+    click_button 'Add Question'
+    expect(page).to have_content('Title must be 40 characters, description must be 150.')
+  end
+
+end
